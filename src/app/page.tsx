ee579d18d7db48655f432f2f75217d8b6f6fb4be
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-06-04 14:38:07 -0500

    :sparkles: add ad component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import InteractionContainer from "@/components/InteractionContainer";
 import Footer from "@/components/Footer";
 import Favicon from "@/components/Favicon";
+import { TincreAd } from "@/components/TincreAd";
 
 export default function Home() {
   return (
@@ -9,6 +10,13 @@ export default function Home() {
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]" />
       <Header />
       <InteractionContainer />
+      <TincreAd
+        {...{
+          url: "https://tincre.com",
+          adtext: "Easiest ads on the web",
+          image: { url: "/tincre_logo_small.jpg", width: 50 },
+        }}
+      />
       <Footer />
       <Favicon />
     </main>
