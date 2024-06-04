92eb1b03563bdbed730138e3ceccc7f569910360
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-06-04 14:02:25 -0500

    :sparkles: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -91,6 +92,7 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-H78K8LP0QK" />
     </html>
   );
 }
