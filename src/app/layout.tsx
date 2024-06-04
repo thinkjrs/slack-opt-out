18b1d1d3903deff0cd839f3ea87814f7d991ebd1
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-06-04 13:27:15 -0500

    :bug: fix nextjs metadata trash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,36 @@ export default function RootLayout({
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
+        <title>noslack.org</title>
+        <meta
+          name="description"
+          content="Say no to Slack's corporate data theft with this opt-out tool."
+        />
+
+        <meta property="og:url" content="https://noslack.org/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="noslack.org" />
+        <meta
+          property="og:description"
+          content="Say no to Slack's corporate data theft with this opt-out tool."
+        />
+        <meta
+          property="og:image"
+          content="https://noslack.org/opengraph-image.jpg"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="noslack.org" />
+        <meta property="twitter:url" content="https://noslack.org/" />
+        <meta name="twitter:title" content="noslack.org" />
+        <meta
+          name="twitter:description"
+          content="Say no to Slack's corporate data theft with this opt-out tool."
+        />
+        <meta
+          name="twitter:image"
+          content="https://noslack.org/twitter-image.jpg"
+        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
